112639060bea67d0e0e22cedec49fb5563de5ce6
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-08-03 18:50:02 +0200

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil');
+});
+
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+});
+
+app.listen(8080);
